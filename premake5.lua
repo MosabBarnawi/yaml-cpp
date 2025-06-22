310958382308fa2628737e02ebb0f0c07db1ad00
MSI\justt <just.t17@gmail.com>
2025-06-22 11:41:38 +0300

    - Update Gitignore file. - Added Premake file

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+	--warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+    	"src/**.h",
+    	"src/**.cpp",
+
+    	"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+    	staticruntime "on"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+    	staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
